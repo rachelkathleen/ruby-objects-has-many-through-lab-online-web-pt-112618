b36c47be3b6f11f407fa447c839b5c817ae5f500
Rachel Hawa <rachel.hawa@gmail.com>
2019-01-11 20:35:28 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,18 @@
+class Artist
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_song(name, artist, genre)
+    Song.new(name, artist, genre)
+  end
+end
---
+++ lib/genre.rb
@@ -0,0 +1,8 @@
+class Genre
+  attr_accessor :name
+  @@all = []
+
+  def self.all
+  end
+
+end
