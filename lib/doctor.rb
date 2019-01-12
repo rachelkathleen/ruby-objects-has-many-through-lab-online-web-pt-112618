ddf789effaea857bf99543436baeb2668a5c4bb3
Rachel Hawa <rachel.hawa@gmail.com>
2019-01-12 01:15:15 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -16,11 +16,11 @@ class Doctor
   end
 
   def appointments
-    Appointment.all.select {|appointment| appointment.doctor == self} 
+    Appointment.all.select {|appointment| appointment.doctor == self}
   end
 
   def patients
-     
+     appointments.map(&:patients)
 
   def genres
     songs.map(&:genre)
