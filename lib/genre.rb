0bb7cc6ecceae6f602724f2c0639e8b98941afba
Rachel Hawa <rachel.hawa@gmail.com>
2019-01-12 01:49:36 +0000

    Done.

--- lib/genre.rb
+++ lib/genre.rb
@@ -11,4 +11,15 @@ class Genre
     @@all
   end
 
+  def songs
+    Song.all { |song| song.genre == self }
+  end
+
+  def artists
+    songs.map(&:artist)
+  end
+
+  def add_song(song)
+    songs << song
+  end
 end
